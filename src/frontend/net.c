224265f17bed96df19613ce0534fb4717ed45c58
techflashYT <officialTechflashYT@gmail.com>
2024-12-04 00:29:46 -0700

    more networking and threading stuff

--- src/frontend/net.c
+++ src/frontend/net.c
@@ -1,3 +1,5 @@
+#include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -9,6 +11,37 @@
 #include <common/packet.h>
 #include <frontend/config.h>
 #include <unistd.h>
+
+static pthread_t connThread[NUM_NODES];
+
+typedef struct param {
+	struct sockaddr_in *addr;
+	int sockfd;
+	int *connfd;
+	int i;
+} param_t;
+
+
+static void *tryConnect(void *arg) {
+	param_t *info = arg;
+	int sockfd = info->sockfd;
+	int *connfd = info->connfd;
+	int i = info->i;
+	struct sockaddr_in *addr = info->addr;
+
+	// now free the info struct, we've copied the data we need to the stack
+	free(info);
+
+	printf("Trying to connect to %s (%s)...\n", nodes[i].description, nodes[i].ip);
+
+	*connfd = connect(sockfd, (struct sockaddr *)addr, sizeof(struct sockaddr_in));
+	if (*connfd < 0) {
+		perror("connect");
+		return NULL;
+	}
+
+	return NULL;
+}
 
 void *startNet(void *arg) {
 	(void)arg;
@@ -24,15 +57,17 @@ void *startNet(void *arg) {
 	struct sockaddr_in addr[NUM_NODES];
 	int connfd[NUM_NODES];
 	for (int i = 0; i < NUM_NODES; i++) {
-		addr[i].sin_family = AF_INET;
-		addr[i].sin_port = htons(nodes[i].port);
-		addr[i].sin_addr.s_addr = inet_addr(nodes[i].ip);
-		connfd[i] = connect(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+		param_t *info = malloc(sizeof(param_t));
+		info->addr = &addr[i];
+		info->sockfd = sockfd;
+		info->connfd = &connfd[i];
+		info->i = i;
 
-		if (connfd[i] < 0) {
-			perror("connect");
-			return NULL;
-		}
+		// we do this in it's own thread because connecting could take ages if the node is down,
+		// and we don't want to block the entire network thread if a node is down.
+		pthread_create(&connThread[i], NULL, tryConnect, info);
+
+		// don't free info here, the thread will do that after it has the info it needs
 	}
 
 	while (true) {
@@ -49,6 +84,10 @@ void *startNet(void *arg) {
 		// handle sending any data necessary back to the main thread, for sending and updating Discord messages
 	}
 
+	for (int i = 0; i < NUM_NODES; i++) {
+		pthread_kill(connThread[i], SIGINT);
+		pthread_join(connThread[i], NULL);
+	}
 	close(sockfd);
 
 	return NULL;
