122df58cd49448213c55e5001fa591645bb05f54
techflashYT <officialTechflashYT@gmail.com>
2024-12-04 00:15:43 -0700

    cleanup

--- src/backend/main.c
+++ src/backend/main.c
@@ -5,12 +5,11 @@
 #include <sys/wait.h>
 #include <backend/net.h>
 static pthread_t netThread;
-static int netId;
 
 int main() {
 	puts("WiiBot Node v" NODE_VER " is starting up...");
 	while (true) {
-		pthread_create(&netThread, NULL, startNet, &netId);
+		pthread_create(&netThread, NULL, startNet, NULL);
 		pthread_join(netThread, NULL);
 
 		printf("\e[1;31mThe network thread exited, restarting...\e[0m\n");
